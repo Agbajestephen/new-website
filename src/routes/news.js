831527a3948c11da472a1651f707858abd4cc62d
agbajestephen <agbajestephen5@gmail.com>
2025-07-02 07:08:10 +0100

    update

---
+++ src/routes/news.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const newsRouter = express.Router()
+const axious = require('axios')
+
+newsRouter.get('', async(req, res) =>{
+    // res.render('news')
+    
+})
+
+module.exports = newsRouter 
