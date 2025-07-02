86771681b919a79302fc1c1da70865292e190025
agbajestephen <agbajestephen5@gmail.com>
2025-07-02 11:10:52 +0100

    update

--- src/routes/news.js
+++ src/routes/news.js
@@ -3,8 +3,20 @@ const newsRouter = express.Router()
 const axious = require('axios')
 
 newsRouter.get('', async(req, res) =>{
-    // res.render('news')
-    
+    try{
+        const newsAPI =await axious.get('https://raddy.dev/wp-json/wp/v2/posts')
+        res.render('news', {articles:newsAPI.data})
+    }catch(err) {
+        if(err.response){
+            console.log(err.response.data)
+            console.log(err.response.status)
+            console.log(err.response.headers)
+        }else if(err.request){
+            console.log(err.request)
+        }else{
+            console.log('Error', err.meassage)
+        }    
+    }
 })
 
-module.exports = newsRouter 
+module.exports = newsRouter
