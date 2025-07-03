a6b7eeea2de7789bcc6cea97002e6492a675472e
agbajestephen <agbajestephen5@gmail.com>
2025-07-03 10:46:47 +0100

    update

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
+// Importing the express module to create a web server
+// Importing body-parser to parse incoming request bodies
+
 const express = require('express')
+const bodyparser = require('body-parser')
 
 const app = express()
 const port = 5000
 
 //stactic files
+// Using express.static middleware to serve static files from the 'public' directory
+// This allows the server to serve CSS, images, and JavaScript files from the specified directories
+
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname +'public/css'))
 app.use('/img', express.static(__dirname +'public/img'))
 app.use('/js', express.static(__dirname +'public/js'))
 
 //templating engine
+// Setting the views directory to './src/views/partials' and using EJS as the templating engine
+
 app.set('views','./src/views/partials')
 app.set('view engine', 'ejs')
 
+//body parser
+
+app.use(bodyparser.urlencoded({extended: true}))
+
 //Routes 
+// Importing the news router from the routes directory
+// This router handles the main news page and individual article pages
+
 const newsRouter = require('./src/routes/news')
 
-app.use('/', newsRouter) 
+// Using the news router for the root path and '/article' path
+// This means that requests to '/' and '/article' will be handled by the newsRouter
+
+app.use('/', newsRouter)
+app.use('/article', newsRouter)
+
+// Error handling
 //listen on port 3000
+
 app.listen(port,() => console.log('listening on port ${port}'))
--- src/routes/news.js
+++ src/routes/news.js
@@ -2,22 +2,82 @@ const express = require('express')
 const newsRouter = express.Router()
 const axious = require('axios')
 
+// This route handles the main news page to fetch all articles from the API and render them.
+// It uses the 'axios' library to make HTTP requests to the WordPress REST API.
+
 newsRouter.get('', async(req, res) =>{
     try{
         const newsAPI =await axious.get('https://raddy.dev/wp-json/wp/v2/posts')
         res.render('news', {articles:newsAPI.data})
     }catch(err) {
         if(err.response){
-             res.render('news', {articles : null})
+            res.render('news', {articles : null})
             console.log(err.response.data)
             console.log(err.response.status)
             console.log(err.response.headers)
         }else if(err.request){
+            res.render('news', {articles : null})
             console.log(err.request)
         }else{
+            res.render('news', {articles : null})
             console.log('Error', err.meassage)
         }    
     }
 })
 
 module.exports = newsRouter
+
+// This route handles the individual news article display to get the article ID from the URL
+// and fetches the article details from the API.
+
+newsRouter.get('/:id', async(req, res) =>{
+    let articleID = req.params.id
+
+    try{
+        const newsAPI =await axious.get(`https://raddy.dev/wp-json/wp/v2/posts/${articleID}`)
+        res.render('newsSingle', {article:newsAPI.data})
+    }catch(err) {
+        if(err.response){
+            res.render('newsSingle', {article : null})
+            console.log(err.response.data)
+            console.log(err.response.status)
+            console.log(err.response.headers)
+        }else if(err.request){
+            res.render('newsSingle', {article : null})
+            console.log(err.request)
+        }else{
+            res.render('newsSingle', {article : null})
+            console.log('Error', err.meassage)
+        }    
+    }
+})
+
+
+newsRouter.post('', async(req, res) =>{
+    let search = req.body.search
+
+    try{
+        const newsAPI =await axious.get(`https://raddy.dev/wp-json/wp/v2/posts?search=${search}`)
+        res.render('newsSearch', {articles:newsAPI.data})
+    }catch(err) {
+        if(err.response){
+            res.render('newsSearch', {articles : null})
+            console.log(err.response.data)
+            console.log(err.response.status)
+            console.log(err.response.headers)
+        }else if(err.request){
+            res.render('newsSearch', {articles : null})
+            console.log(err.request)
+        }else{
+            res.render('newsSearch', {articles : null})
+            console.log('Error', err.meassage)
+        }    
+    }
+})
+
+
+
+
+module.exports = newsRouter
+
+
